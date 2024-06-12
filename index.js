406310252fe51ff3860dfa3aff9d8122a9274a3c
StephOg <stephanieogwo@Stephanies-MacBook-Pro.local>
2024-06-11 23:52:40 -0400

    finalized result function

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeTheQuiz(ev) {
 
 }
 
-function loadResult() { //helperfunction using doc frag to load the result depending on the answers to the quiz.
+function loadResult() { //helperfunction using doc frag to load the result depending on the answers to the quiz. then add to page.
     const answerScore = (((Number(document.getElementById('birthMonth').value)) / Number(document.querySelector('input[name="spiritAnimal"]:checked').value)) / 2);
     if (answerScore <= 0.6) {
         const textResult = "I am getting better and better.\nI am smart and capable of being a winner.\nI am an abundant being.\nI allow the flow of abundance in and through me.\nAll my problems have solutions.\nI have enough. I am enough.";
@@ -16,8 +16,13 @@ function loadResult() { //helperfunction using doc frag to load the result depen
     else {
         const textResult = "I am proud of myself.\nI am full of potential.\nI am naturally confident and at ease in my own life.\nI bring value to the people in my life.\nI have natural good fortune and boundless opportunity.\nMy dreams are coming true.";
     }
+    const resultContainer = document.createElement('div');
+    resultContainer.setAttribute("id", "resultBox");
+    resultContainer.textContent = textResult;
 
-    const fragResult = document.createDocumentFragment();
+    const fragResult = document.createDocumentFragment(); //make template
+    fragResult.appendChild(resultDiv);
+    document.getElementById('app').appendChild(fragResult); //add result to webpage
 }
 
 //helper functions - function to unhide each question
